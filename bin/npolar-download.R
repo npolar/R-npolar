8ff735bea47bcece60c1f75e91077250f92d94e6
cnrdh <cnrdh@users.noreply.github.com>
2015-06-19 09:59:37 +0200

    More examples and post-save counting

--- bin/npolar-download.R
+++ bin/npolar-download.R
@@ -1,6 +1,6 @@
 # npolar-download.R 
-#   Download an entire Npolar API collection to disk, sliced in one document per year, month, or day (interval)
-#
+#   Download an entire Npolar API collection to disk. Slices the collection in one document per year, month, or day (interval).
+#   
 # Usage:
 #  Rscript npolar-download.R {relative API path} {destination} {format} {interval} {intervalField}
 #
@@ -8,15 +8,30 @@
 #   Set R_NPOLAR_USERNAME and R_NPOLAR_USERNAME (only needed if the API endpoint has restricted access)
 # 
 # Examples
-# JSON replica of Dataset API organised by year created
-#   ➜ Rscript ./bin/npolar-download.R /dataset ./api.npolar.no json year created
+# * JSON replica of Dataset API organised by year created
+# ✗ Rscript ./bin/npolar-download.R /dataset ./api.npolar.no json year created
 #
-# CSV download of Svalbard Reindeer GPS tracks, with 1 document per month
-#   ➜ Rscript ./bin/npolar-download.R /tracking/svalbard-reindeer /tmp/api.npolar.no csv month measured "measured,platform,latitude,longitude,altitude,activity_y,activity_x,hdop,temperature,time_to_fix,satellites,comment"
+# * CSV download of Svalbard Reindeer GPS tracks, with 1 document per month, and specified fields only
+# ✗ Rscript ./bin/npolar-download.R /tracking/svalbard-reindeer /tmp/api.npolar.no csv month measured "measured,platform,latitude,longitude,altitude,activity_y,activity_x,hdop,temperature,time_to_fix,satellites,comment"
+#
+# * oceanography buoy JSON
+# ✗ Rscript ./bin/npolar-download.R /oceanography/buoy /tmp/api.npolar.no
+# Starting json download of /oceanography/buoy interval month measured to /tmp/api.npolar.no
+# 37349 remote documents, most recent measured 
+#  [{"measured":["2015-06-15T12:00:00Z"],"links":[{"title":["Deployment logs"],"rel":["log"],"href":["http://data.npolar.no/raw/buoy/deployment-logs/"]}],"collection":["buoy"],"type":["MetOcean i-svp"],"schema":["http://api.npolar.no/schema/oceanography_point-1.0.1"],"_rev":["1-1d734b7d88d06ffc85f0bcc1748902b5"],"id":["d35bd0da-7df2-5220-9d14-0e9ed8cefb75"],"title":["SVP_2015e"],"sea_surface_temperature":[2.8],"_id":["d35bd0da-7df2-5220-9d14-0e9ed8cefb75"],"IMEI":["300234010084440"],"air_pressure_tendency":[0.8],"owner":["FMI"],"air_pressure":[1010],"deployment":{"longitude":[15.0496],"latitude":[83.05],"date":["2015-04-20T10:00:00Z"]},"longitude":[-2.2954],"latitude":[77.2774]}]
+# 6 month intervals using measured field
+# Counts in existing local copy of 2015-01 matches API /oceanography/buoy count: 2171
+# Counts in existing local copy of 2015-02 matches API /oceanography/buoy count: 6968
+# Counts in existing local copy of 2015-03 matches API /oceanography/buoy count: 6290
+# Counts in existing local copy of 2015-04 matches API /oceanography/buoy count: 5267
+# Counts in existing local copy of 2015-05 matches API /oceanography/buoy count: 12427
+# Fetching 2015-06 [ month 6 / 6 ] using http://api.npolar.no/oceanography/buoy?limit=1&variant=atom&q=&format=json&date-month=measured&sort=-measured&filter-measured=2015-06-01T00:00:00Z..2015-07-01T00:00:00Z&format=json&sort=measured&limit=all&variant=array
+# Saving /tmp/api.npolar.no//oceanography/buoy/json/month-measured/2015-06-oceanography-buoy-npolar.json
+# Counts in fresh local copy of 2015-06 matches API /oceanography/buoy count: 4226
 #
 # Dependencies
-# * [httr](https://github.com/hadley/httr)
-# * [jsonlite]
+# * [httr](https://github.com/hadley/httr) - [quickstart](http://cran.r-project.org/web/packages/httr/vignettes/quickstart.html)
+# * [jsonlite](https://github.com/jeroenooms/jsonlite) - [The jsonlite Package: A Practical and Consistent Mapping Between JSON Data and R Objects](http://arxiv.org/pdf/1403.2805v1.pdf)
 #
 # Notice: This is an initial draft version
 api.get <- function(uri, headers) {
@@ -24,7 +39,7 @@ api.get <- function(uri, headers) {
   username <- Sys.getenv("R_NPOLAR_USERNAME")
   password <- Sys.getenv("R_NPOLAR_PASSWORD")
   
-  response <- httr::GET(uri, authenticate(username, password, "basic")) # set_cookies(), add_headers(), authenticate(), use_proxy(), verbose(), timeout(), content_type(), accept(), progress().
+  response <- httr::GET(uri, authenticate(username, password, "basic"))
   if (response$status_code > 299) {
     stop(paste("GET request failed with status", response$status_code, "for", uri, "\n", response))
   }
@@ -36,7 +51,7 @@ api.json <- function(uri) {
   jsonlite::fromJSON(json, simplifyVector = FALSE, simplifyDataFrame = FALSE)
 }
 
-checkCount <- function(filename, format) {
+count <- function(filename, format) {
   
   count <- 0
   if (grepl("csv", format)) {  
@@ -44,8 +59,7 @@ checkCount <- function(filename, format) {
   } else if (grepl("json", format)) {
     count <- length(fromJSON(filename, simplifyVector = FALSE, simplifyDataFrame = FALSE))
   }
-  count
-  
+  count 
 }
       
 intervalFacet <- function(feed, term) {
@@ -70,21 +84,18 @@ intervalFacet <- function(feed, term) {
 
 
 library(jsonlite)
-library(httr) # http://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+library(httr)
 
 args <- commandArgs(TRUE)
 
-# if  != "/") == TRUE {
-#   stop("Usage: Rscript npolar-fetch.R /api/path")
-# }
 base <- "https://api.npolar.no"
 
-path <- args[1] # "/dataset"
-destination <- args[2] # "."
+path <- args[1]
+destination <- args[2]
 format <- args[3]
-interval <- args[4] # "month"
-intervalField <- args[5] #"measured"
-fields <- args[6] # measured,platform,latitude,longitude,altitude,activity_y,activity_x,hdop,temperature,time_to_fix,satellites,comment
+interval <- args[4]
+intervalField <- args[5]
+fields <- args[6]
 
 usage = "Usage:\nRscript npolar-download.R {relative API path} [{destination} {format} {interval} {intervalField}]"
 
@@ -93,7 +104,6 @@ if (is.na(destination)) { destination <- "./api.npolar.no" }
 if (is.na(format)) { format <- "json" }
 if (is.na(intervalField)) { intervalField <- "measured" }
 if (is.na(interval)) { interval <- "month" }
-#if (is.na(fields)) { }
 
 query <- paste0("?q=&format=json&variant=atom&limit=1&date-",interval,"=",intervalField,"&sort=-",intervalField)
 uri <- paste0(base,path,query)
@@ -107,7 +117,7 @@ j = 0
 term <- paste0(interval, "-", intervalField)
 
 facets = intervalFacet(feed, term)
-message(paste(length(facets), interval, "intervals using", intervalField))
+message(paste(length(facets), interval, "intervals using", intervalField, "field"))
 
 for (facet in facets) {
   
@@ -128,8 +138,8 @@ for (facet in facets) {
   
   download <- TRUE
   if (file.exists(filename)) {
-    if (checkCount(filename, format) == facet$count) {
-      message(paste("Counts in local copy of", facet$term, "matches API", path, "count:", facet$count))
+    if (count(filename, format) == facet$count) {
+      message(paste("Counts in existing local copy of", facet$term, "matches API", path, "count:", facet$count))
       download <- FALSE
     }
   }
@@ -145,5 +155,11 @@ for (facet in facets) {
     }
     message(paste("Saving", filename))
     cat(body, file=filename)
+    
+    if (count(filename, format) == facet$count) {
+      message(paste("Counts in fresh local copy of", facet$term, "matches API", path, "count:", facet$count))
+    } else {
+      # noop: Need CSV count to emit proper warning
+    }
   }
 }
