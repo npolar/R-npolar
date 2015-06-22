74956d23933881d53dcd33a89a031e8b1532ce97
cnrdh <cnrdh@users.noreply.github.com>
2015-06-22 11:22:28 +0200

    Relative path in api.get; created api.get.json

--- R/api.R
+++ R/api.R
@@ -15,6 +15,8 @@ api.get <- function(uri, headers) {
   username <- api.security.password()
   password <- api.security.password()
 
+  if (FALSE == grepl("https?://", uri)) { uri <- paste0(api.base, uri) }
+
   response <- httr::GET(uri, authenticate(username, password, "basic"), timeout(30))
   if (response$status_code > 299) {
     stop(paste("GET request failed with status", response$status_code, "for", uri, "\n", response))
@@ -22,7 +24,8 @@ api.get <- function(uri, headers) {
   httr::content(response, "text")
 }
 
-api.json <- function(uri) {
+api.get.json <- function(uri) {
   json <- api.get(uri)
   jsonlite::fromJSON(json, simplifyVector = FALSE, simplifyDataFrame = FALSE)
 }
+
