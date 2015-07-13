386e27b56d0ba04e4ef45b9c6bf1aa02cc9a72b9
cnrdh <cnrdh@users.noreply.github.com>
2015-07-13 15:08:20 +0200

    Cleaned a bit

--- R/api/tracking.R
+++ R/api/tracking.R
@@ -1,3 +1,5 @@
+# Experimental/proof-of-concept tracking processor
+
 api.tracking.processor <- function(tracks) {
   i <- 0
   message(paste("Processing", length(tracks), "documents"))
@@ -12,18 +14,12 @@ api.tracking.processor <- function(tracks) {
 
 }
 
+# Just a proof-of-concept function
 api.tracking.merge <- function(track, metadata) {
   track[[1]]$platform_name = "platform_name"
   track[[1]]$individual = "individual"
   track
 }
 
-api.tracking.deployments <- function() {
-  api.json("/tracking/deployment/?q=&filter-species=Rangifer+tarandus+platyrhynchus&filter-provider=followit.se")
-}
-
-
-
-# tracking download
-#call ytracking depl and then download for eac deplpyment,
-# (holding individ and platform_name => merge in for easch doc)
+# Reindeer
+# "/tracking/deployment/?q=&filter-species=Rangifer+tarandus+platyrhynchus&filter-provider=followit.se"
