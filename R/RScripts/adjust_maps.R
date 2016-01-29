648d3fc27511be7ed79ae0152b5a0c3767d790fe
Siri <siri@nibba.nett.npolar>
2016-01-29 14:40:22 +0100

    draft script aligning two maps

---
+++ R/RScripts/adjust_maps.R
@@ -0,0 +1,37 @@
+# This script takes two raster images, one c smaller than the other s.
+# C is padded so that each pixel represent the same distance and then 
+# enlaged so it fits s in size.
+# The two are the stacked on top of each other and displayed both as 
+# separate layers and as a sinle image.
+
+
+# load libraries
+library(raster)
+c <- raster("vegetasjonskart.png")
+plot(c)
+readline("Press <Enter> to continue")
+
+#Expand the base array - add minus x to min and +x to max value
+ext <- extent(-20,699,-20,729)
+expanded = extend(c, ext, value=0)
+plot(expanded)
+
+#Enlarge the map twice the size
+p = disaggregate(c,2) 
+plot(p,main="twice the size")
+
+s=raster(nrow=40, ncol=40)
+
+#Overlay the å legge punktene over på kartet
+r <- overlay(s, p, fun=function(x,y){return(x+y)})
+a = unstack(r)
+
+#Plot both layers separately
+plot(a)
+
+#View both layers as an image -sandwich style
+image(a) 
+
+
+# unload the libraries
+detach("package:raster")
