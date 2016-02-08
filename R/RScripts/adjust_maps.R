779fb74e87be7dba474c903d8933959c5cfa5057
srldl <siri.uldal@gmail.com>
2016-02-08 15:59:30 +0100

    resample update

--- R/RScripts/adjust_maps.R
+++ R/RScripts/adjust_maps.R
@@ -1,36 +1,28 @@
 # This script takes two raster images, where C is smaller than S.
-# C is padded so that each pixel represent the same distance in meters 
+# C is padded so that each pixel represent the same distance in meters
 # and afterwards enlaged to fit S in size.
-# The two are then stacked on top of each other and displayed both as 
+# The two are then stacked on top of each other and displayed both as
 # separate layers and as a single image.
 
 
 # load libraries
 library(raster)
-c <- raster("vegetasjonskart.png")
+c <- raster("image.png")
 plot(c)
 readline("Press <Enter> to continue")
 
-#Expand the base array - add minus x to min and +x to max value
-ext <- extent(-20,699,-20,729)
-expanded = extend(c, ext, value=0)
-plot(expanded)
+#Expand base array (padding), here 1 pixel each side
+p <- extend(r, c(1,1))
 
-#Enlarge the map twice the size
-p = disaggregate(c,2) 
-plot(p,main="twice the size")
+#Create a new, empty raster with the final dimensions
+s <- raster(nrow=2000, ncol=2000)
 
-s=raster(nrow=40, ncol=40)
-
-#Overlay the å legge punktene over på kartet
-r <- overlay(s, p, fun=function(x,y){return(x+y)})
-a = unstack(r)
-
-#Plot both layers separately
-plot(a)
-
-#View both layers as an image -sandwich style
-image(a) 
+# Resample old extended picture into new raster with correct dimensions
+# ngb is nearest neighbor, need this one rather than bilinear so
+# values are not changed..
+s <- resample(ext, s, method='ngb')
+#result in s
+plot(s)
 
 
 # unload the libraries
